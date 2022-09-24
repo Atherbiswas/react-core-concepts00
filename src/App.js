d14439cfc1e1103a1c0a95f291212d22f6f970f0
Ather1230 <ather.biswas0057@gmail.com>
2022-09-24 07:18:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
 
+// option-1
+// function App() {
+//   return (
+//     <div className="App">
+//       <Person name="Rahim" nayka="mousumi"></Person>
+//       <Person name="Bapparaz" nayka="cheka"></Person>
+//       <Person name="Kuber" nayka="Kopila"></Person>
+//       <h5>New component</h5>
+//       <Friend></Friend>
+//     </div>
+//   );
+// }
+
+// function Person(props){
+//   return (
+//     <div className='person'>
+//       <h1>Name: {props.name}</h1>
+//       <p>Nayka: {props.nayka}</p>
+//     </div>
+//   )
+// }
+
+// function Friend(){
+//   return (
+//     <div className='container'>
+//       <h2>Singho mama</h2>
+//       <p>This lion</p>
+//     </div>
+//   )
+// }
+
+// opotion-2
+const nayoks = ['Riyaz', 'Razzak', 'Bappada', 'Jasim', 'Anowar']
+const singers = [
+  {name: 'Mahfuz', job: 'singer'},
+  {name: 'Eva', job: 'singer1'},
+  {name: 'Agun', job: 'singer2'}
+]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
+      <h5>New component</h5>
+      {
+        singers.map(singer => <Friend name={singer.name} job={singer.job}></Friend>)
+      }
+      
     </div>
   );
 }
 
+function Person(props){
+  return (
+    <div className='person'>
+      <h1>Name: {props.name}</h1>
+      <p>Nayka: {props.nayka}</p>
+    </div>
+  )
+}
+
+function Friend(props){
+  return (
+    <div className='container'>
+      <h2>{props.name}</h2>
+      <p>{props.job}</p>
+    </div>
+  )
+}
 export default App;
